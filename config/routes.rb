22a1a6c7601dfc927ed918ac43c3fdd127df8719
erugis0345 <tl091264@gmail.com>
2018-10-23 23:39:27 +0900

    ユーザー認証機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  if Rails.env.development? # 開発環境の場合
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
