97e6c3adf673d97a50af2ed4ea20c2113f081200
erugis0345 <tl091264@gmail.com>
2018-10-27 17:56:57 +0900

    グループ機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   end
 
   devise_for :users
+  resources :groups
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
