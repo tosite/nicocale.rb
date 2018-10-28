803d734a3b68dc4bf4ab47969a35bbcf0b2ca8a1
erugis0345 <tl091264@gmail.com>
2018-10-29 00:46:24 +0900

    Rubocop忘れてた

--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -5,12 +5,12 @@ class GroupUser < ApplicationRecord
   has_one :user
   validate :exist_group_id
   validate :exist_user_id
-  
+
   private
-  def exist_group_id
-    errors.add(:group_id, "group_idが存在しません。") if Group.all.ids.exclude?(group_id)
-  end
-  def exist_user_id
-    errors.add(:user_id, "user_idが存在しません。") if User.all.ids.exclude?(user_id)
-  end
+    def exist_group_id
+      errors.add(:group_id, "group_idが存在しません。") if Group.all.ids.exclude?(group_id)
+    end
+    def exist_user_id
+      errors.add(:user_id, "user_idが存在しません。") if User.all.ids.exclude?(user_id)
+    end
 end
