af31f64eb0358f51cdd973587ab028fbf09e6ef7
tosite0345 <tl091264@gmail.com>
2018-11-13 11:56:22 +0900

    ユーザーテーブル変更

--- db/migrate/20181023135605_devise_create_users.rb
+++ db/migrate/20181023135605_devise_create_users.rb
@@ -4,7 +4,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: false, default: "", unique: true
+      t.string :account_id,         null: false, default: "", unique: true
+      t.string :name,               null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
--- db/schema.rb
+++ db/schema.rb
@@ -68,6 +68,8 @@ ActiveRecord::Schema.define(version: 2018_11_11_074640) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
+    t.string "account_id", default: "", null: false
+    t.string "name", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
