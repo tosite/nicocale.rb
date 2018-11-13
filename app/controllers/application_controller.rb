92f0ead2dd53173b28be83e1950ed63069e4ddf3
tosite0345 <tl091264@gmail.com>
2018-11-13 20:13:56 +0900

    Usersにアバターを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,7 @@ class ApplicationController < ActionController::Base
   end
 
   def add_permit_params
-    added_attrs = [:name, :account_id, :email, :password, :password_confirmation]
+    added_attrs = [:name, :account_id, :email, :password, :password_confirmation, :avatar]
     devise_parameter_sanitizer.permit :sign_up,        keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in,        keys: added_attrs
