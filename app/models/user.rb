8ca0ebcb94fff1a11fc26320c7b109804028d566
tosite0345 <tl091264@gmail.com>
2018-11-14 10:52:37 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,20 @@ class User < ApplicationRecord
   has_one_attached :avatar
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise   :database_authenticatable, :registerable,
-           :recoverable, :rememberable, :validatable,
-           :confirmable, :trackable
-  has_many :team_users, dependent: :destroy
-  has_many :user_emotions, dependent: :destroy
+  devise    :database_authenticatable,   :registerable,
+            :recoverable, :rememberable, :validatable,
+            :confirmable, :trackable
+  has_many  :team_users,    dependent: :destroy
+  has_many  :user_emotions, dependent: :destroy
+
+  validates :account_id,
+              presence:   true,
+              length:     { maximum: 15 }, # Twitter
+              uniqueness: true
+
+  validates :name,
+              presence: true,
+              length:   { maximum: 50 } # Twitter
 
   def team_user(team_id)
     self.team_users.find_by(team_id: team_id)
