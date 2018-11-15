509bfefde54e6162b8a399b9acdb9fc238da2061
tosite0345 <tl091264@gmail.com>
2018-11-15 11:59:40 +0900

    Controller側でカレンダーを生成するようにして速度改善

--- app/controllers/emotions/list_controller.rb
+++ app/controllers/emotions/list_controller.rb
@@ -13,12 +13,10 @@ class Emotions::ListController < ApplicationController
     @team          = Team.find(params[:team_id])
     @month         = Date.parse("#{params[:id]}01")
     @team_users    = TeamUser.eager_load(:user).team_id(@team.id).all
-    @own_emotions  = UserEmotion.eager_load(:emotion).team_id(@team_id).user_id(current_user.id).reported_on_between(@month).all
-    @user_emotion  = UserEmotion.new
-
-    @calendar_for_me     = set_calendar(@month, current_user.team_users.team_id(@team.id).first, @team.id)
-    @calendar_for_member = {}
-    @team_users.each { |u| @calendar_for_member[u.user_id] = set_calendar(@month, u, @team.id) }
+    @own_emotions  = UserEmotion.eager_load(:emotion).team_id(@team.id).user_id(current_user.id).reported_on_between(@month).all
+    @calendar      = {}
+    @me            = current_user.team_users.team_id(@team.id).first
+    @team_users.each { |u| @calendar[u.user_id] = set_calendar(@month, u, @team.id) }
   end
 
   private
@@ -31,10 +29,10 @@ class Emotions::ListController < ApplicationController
       end
     end
 
-    def set_calendar(month, user, team_id)
+    def set_calendar(month, team_user, team_id)
       calendar = {}
       (month..month.end_of_month).each { |day| calendar[day.strftime("%Y-%m-%d")] = {} }
-      emotions = user.user_emotions.eager_load(:emotion).reported_on_between(@month).all
+      emotions = team_user.user_emotions.eager_load(:emotion).reported_on_between(@month).all
       emotions.each { |e| calendar[e.reported_on.strftime("%Y-%m-%d")] = e }
       calendar
     end
